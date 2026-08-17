7696ad6d04d72da0122f594a9290ef2846d4fd4d
Yiyou Liu <161186139+TheUsername1214@users.noreply.github.com>
2026-08-17 16:37:35 -0400

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -22,16 +22,16 @@ CREATE TABLE IF NOT EXISTS stations (
 );
 
 CREATE TABLE IF NOT EXISTS orders (
-                                      order_id           INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-                                      user_id            INTEGER NOT NULL REFERENCES users (id),
-                                      destination        VARCHAR(500) NOT NULL,
-                                      package_weight_lbs DOUBLE PRECISION NOT NULL CHECK (package_weight_lbs > 0),
-                                      price              DOUBLE PRECISION NOT NULL CHECK (price >= 0),
-                                      time               DOUBLE PRECISION NOT NULL CHECK (time >= 0),
-                                      vehicle            VARCHAR(20) NOT NULL CHECK (vehicle IN ('ROBOT', 'DRONE')),
-                                      station_id         INTEGER NOT NULL REFERENCES stations (station_id),
-                                      status             VARCHAR(30) NOT NULL DEFAULT 'PENDING_DROPOFF'
-                                          CHECK (status IN ('PENDING_DROPOFF', 'AT_STATION', 'BEFORE_HALF_WAY',
-                                                            'HALF_WAY', 'MORE_THAN_HALF_WAY', 'DELIVERED', 'CANCELLED')),
-                                      created_at         TIMESTAMPTZ NOT NULL DEFAULT now()
+                        order_id           INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                        user_id            INTEGER NOT NULL REFERENCES users (id),
+                        destination        VARCHAR(500) NOT NULL,
+                        package_weight_lbs DOUBLE PRECISION NOT NULL CHECK (package_weight_lbs > 0),
+                        price              DOUBLE PRECISION NOT NULL CHECK (price >= 0),
+                        time DOUBLE PRECISION NOT NULL CHECK (time >= 0),
+                        vehicle            VARCHAR(20) NOT NULL CHECK (vehicle IN ('ROBOT', 'DRONE')),
+                        station_id         INTEGER NOT NULL REFERENCES stations (station_id),
+                        status             VARCHAR(30) NOT NULL DEFAULT 'PENDING_DROPOFF'
+                            CHECK (status IN ('PENDING_DROPOFF', 'AT_STATION', 'BEFORE_HALF_WAY',
+                                              'HALF_WAY', 'MORE_THAN_HALF_WAY', 'DELIVERED', 'CANCELLED')),
+                        created_at          TIMESTAMPTZ NOT NULL DEFAULT now()
 );
